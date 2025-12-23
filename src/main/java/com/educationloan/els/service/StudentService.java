af20bd91865fbe19bac76468fdacfc15b1f9655f
Sanket Darunkar <sanketdarunkar111@gmail.com>
2025-12-23 23:36:58 +0530

    Add placeholders to track package structure

---
+++ src/main/java/com/educationloan/els/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.educationloan.els.service;
+
+public interface StudentService {
+}
---
+++ src/main/java/com/educationloan/els/service/impl/StudentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.educationloan.els.service.impl;
+
+import com.educationloan.els.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
