ef9d1465ce9953bf062a417ef80aa10e6e14e75b
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-14 18:26:28 +0530

    Update DocumentController.java
    Api changed Added /student

--- src/main/java/com/els/educationloansystem/controller/DocumentController.java
+++ src/main/java/com/els/educationloansystem/controller/DocumentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/documents")
+@RequestMapping("api/student/documents")
 @CrossOrigin
 public class DocumentController {
 
