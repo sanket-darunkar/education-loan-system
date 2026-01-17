863c882dc993946b9148f99ea2581f50cf0403d7
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-18 01:56:45 +0530

    Update LoanApplicationRequest.java

--- src/main/java/com/els/educationloansystem/dto/LoanApplicationRequest.java
+++ src/main/java/com/els/educationloansystem/dto/LoanApplicationRequest.java
@@ -1,15 +1,16 @@
 package com.els.educationloansystem.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoanApplicationRequest {
-
-    private Long studentId;
 
     private Double loanAmount;
     private String courseName;
     private String instituteName;
     private Integer courseDuration;
 }
-
