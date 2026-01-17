8549ef04fc98788d1337225c3857c2e8d8e147e1
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-18 04:10:55 +0530

    Update LoanApplicationRepository.java

--- src/main/java/com/els/educationloansystem/repository/LoanApplicationRepository.java
+++ src/main/java/com/els/educationloansystem/repository/LoanApplicationRepository.java
@@ -1,6 +1,8 @@
 package com.els.educationloansystem.repository;
 
 import com.els.educationloansystem.entity.LoanApplication;
+
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -23,4 +25,7 @@ public interface LoanApplicationRepository
         ORDER BY la.applicationDate
     """)
     List<Object[]> countApplicationsGroupedByDate();
+
+	@Nullable
+	Object findByApplicationStatus(String string);
 }
