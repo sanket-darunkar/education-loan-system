cd70660e9ae129a6d803ab71ef9a3f521b8db8d9
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-30 15:42:41 +0530

    Update LoanRepository.java

--- src/main/java/com/els/educationloansystem/repository/LoanRepository.java
+++ src/main/java/com/els/educationloansystem/repository/LoanRepository.java
@@ -2,12 +2,10 @@ package com.els.educationloansystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.els.educationloansystem.entity.Loan;
-import com.els.educationloansystem.entity.LoanEligibility;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-	void save(LoanEligibility entity);
+    boolean existsByApplication_ApplicationId(Long applicationId);
 }
